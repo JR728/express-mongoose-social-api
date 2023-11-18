cf61b75af34e3801ab67ef8ce3d1d3271b8695ae
JR728 <135184435+JR728@users.noreply.github.com>
2023-11-17 22:34:27 -0600

    added apps and update server to incude it

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,16 @@
 //routes/index.js
+const router = require('express').Router();
+const userRoutes = require('./api/userRoutes');
+const thoughtRoutes = require('./api/thoughtRoutes');
+const reactionRoutes = require('./api/reactionRoutes');
+
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
+router.use('/thoughts/:thoughtId/reactions', thoughtRoutes); // Add this line for nested reactions route
+router.use('/reactions', reactionRoutes);
+
+router.use((req, res) => {
+  return res.status(404).json({ message: 'Wrong route!' });
+});
+
+module.exports = router;
