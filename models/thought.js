363ae854a6acc53313282cb3b497c96feac04abc
JR728 <135184435+JR728@users.noreply.github.com>
2023-11-17 18:38:17 -0600

    mongoose db

--- models/thought.js
+++ models/thought.js
@@ -1,1 +1,29 @@
 //models/thought.js
+const mongoose = require('mongoose');
+
+const thoughtSchema = new mongoose.Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAt) => new Date(createdAt).toLocaleString(),
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reaction' }],
+});
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ utils/seeds.js
@@ -0,0 +1,1 @@
+//utils/seeds.js
